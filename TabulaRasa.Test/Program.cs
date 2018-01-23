762cff9009dc69b25bd6331f185ed550ee034a97
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2018-01-23 11:39:05 -0800

    Initial commit

---
+++ TabulaRasa.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TabulaRasa.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
